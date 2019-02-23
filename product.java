40482d7dd6d1e182b873a3cffa8d0008067636be
Arman Hakim Sagar <armanhakimsagar@gmail.com>
2019-02-23 11:17:26 +0600

    Create product.java

---
+++ product.java
@@ -0,0 +1,18 @@
+
+package javaapplication8;
+
+public class product {
+    private int id;
+    private String name;
+    
+    public product(String pname){
+        this.name = pname;
+    }
+    
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+}
